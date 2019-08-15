e73c2dad7083e150585bc6908e29c070f310632a
Oleg Dudkin <dudkin.on@gmail.com>
2019-08-15 14:16:59 -0700

    refactor: Add opotunity to change paramerts in validateBody middlewre

--- server/middleware/validateBody.js
+++ server/middleware/validateBody.js
@@ -1,13 +1,13 @@
-module.exports = (validator) => {
+module.exports = (validator, property='body', key='value') => {
   /**
    * Joi validator middleware. Get validator function as parameter
    * and return middleware function, which validate client data
    */
 
   return (req, res, next) => {
-    const { error, value } = validator(req.body);
+    const { error, value } = validator(req[property]);
     if (error) return res.status(400).send({error: error.details[0].message});
-    req.value = value;
+    req[key] = value;
     next();
   }
 };
