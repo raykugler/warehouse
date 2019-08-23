1d67434a1ca60c6526a0257e691b82974c89ed49
Oleg Dudkin <dudkin.on@gmail.com>
2019-08-23 15:29:44 -0700

    feat: Update routes router

--- server/routes/controllers/routes.js
+++ server/routes/controllers/routes.js
@@ -1,17 +1,24 @@
 const {Routes} = require('../../models/routes');
+const {Location} = require('../../models/locations');
 const config = require('config');
 
 
 module.exports = {
   get: async (req, res) => {
     /**
+     * Returns an array of routes
+     */
+    return res.send(await Routes.getAllRoutes())
+  },
+  getById: async (req, res) => {
+    /**
      * Returns current routes
      */
-    return res.send(await Routes.getCurrent())
+    return res.send(await Routes.getById(req.params.id));
   },
   upload: async (req, res) => {
     /**
-     * return routes object
+     * Return create an array of routes
      */
 
     await Routes.deleteAll();
@@ -20,20 +27,30 @@ module.exports = {
     return res.send(routes);
   },
 
-  post: async (req, res) => res.send(await Routes.create(req.values)),
+  post: async (req, res) => {
+    /**
+     * Create a new route
+     */
+    const _route = await Routes.getByRouteCode(req.value.routeCode);
+    if (_route) return res.send(_route);
+
+    return res.send(await Routes.create(req.value))
+  },
 
   put: async (req, res) => {
-    const {_id} = req.params;
-    if (!_id) return res.status(400)
-      .send({error: config.get('errors.routes.errc2')});
 
+    const old_route = await Routes.getById(req.body._id);
+    const conflict_route = await Routes.getByRouteCode(req.body.routeCode);
 
-    const route = await Routes.update(req.route);
-
-    if (!route) return res.status(400)
+    if (!old_route) return res.status(400)
       .send({error: config.get('errors.routes.errc3')});
 
-    return res.send(route);
+    if (conflict_route && old_route.routeCode !== req.body.routeCode)
+      return res.status(400).send({
+        error: config.get('errors.routes.errc6')
+      });
+
+    return res.send(await Routes.update(req.body));
   },
   delete: async (req, res) => {
     return res.send(await Routes.deleteAll());
